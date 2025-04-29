82b0337af8fae2650c8992cffcd34168f03de236
NARENDRA9959 <cnarendra329@gmail.com>
2025-04-29 21:44:06 +0530

    Homepage Added

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Home from './pages/Home/Home';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+export default function App() {
+  return (
+    <Home />
+  )
+}
---
+++ frontend/src/components/Categories/Categories.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import categoriesData from '../../data/Categories.json';
+
+export default function Categories() {
+  return (
+      <div className='container'>
+          <div>
+              <span className='fw-bold fs-5'>Categories</span>
+          </div>
+          <div className='row'>
+              {
+                  categoriesData.map((category) => {
+                      return <div className='col-md-3'>
+                            <div className='card shadow mt-3'>
+                                <img src={category.imageUrl} alt={category.name} width="100%" height="200"/>
+                                <div className='text-center'>
+                                    <span className='fs-5 fw-bold'>{category.name}</span>
+                                </div>
+                            </div>
+                        </div>
+                  })
+              }
+          </div>
+    </div>
+  )
+}
